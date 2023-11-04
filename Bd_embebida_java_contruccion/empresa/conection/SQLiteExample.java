6a5df1adda24d5f8cd5e2987927d2bb6d582dc64
Elton Saravia <ryuzaki12elton@gmail.com>
2023-11-04 15:03:43 +0100

    intentando conexion con sqlite

---
+++ Bd_embebida_java_contruccion/empresa/conection/SQLiteExample.java
@@ -0,0 +1,30 @@
+package conection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class SQLiteExample {
+    public static void main(String[] args) {
+        try {
+            // Establecer la conexión a la base de datos SQLite
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:./BD.db");
+
+            // Crear una tabla
+            Statement statement = connection.createStatement();
+            statement.execute(
+                    "CREATE TABLE IF NOT EXISTS empleados (id INTEGER PRIMARY KEY, nombre TEXT, salario REAL)");
+
+            // Insertar datos
+            statement.execute("INSERT INTO empleados (nombre, salario) VALUES ('Juan', 50000.0)");
+
+            // Realizar consultas
+            // ...
+
+            // Cerrar la conexión
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
