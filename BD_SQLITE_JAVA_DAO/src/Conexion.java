95650d4eb13e47a0f9c8b6cc41fd85d9c4b5554c
Elton Saravia <ryuzaki12elton@gmail.com>
2023-11-06 19:46:58 +0100

    sin arreglar conexiona bd

--- BD_SQLITE_JAVA_DAO/src/Conexion.java
+++ BD_SQLITE_JAVA_DAO/src/Conexion.java
@@ -7,8 +7,7 @@ public class Conexion {
     public Connection conectar() {
         try {
             Class.forName("org.sqlite.JDBC");
-            conectar = DriverManager.getConnection("jdbc:sqlite:BD.db");
-
+            conectar = DriverManager.getConnection("jdbc:sqlite:BD_SQLITE_JAVA_DAO/BD.db");
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Error al conectar a la base de datos.");
