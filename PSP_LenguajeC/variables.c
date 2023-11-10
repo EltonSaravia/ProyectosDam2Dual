b8b5debfb6a5a055746ee23ef464c36f89ba9796
Elton Saravia <ryuzaki12elton@gmail.com>
2023-11-10 11:15:48 +0100

    Update variables.c

---
+++ PSP_LenguajeC/variables.c
@@ -0,0 +1,16 @@
+/*
+    Variables
+*/
+#include <stdio.h>
+int main(){
+    char C = 'y'; //tamaño de 1 byte 
+    int a = 20; //2 bytes  -32768...32767
+    short e = -1; //2 bytes -128...128
+    unsigned int u = 25; // 2 bytes 0...65535
+    long l = 5932; // 4 bytes -2147483648...2147483647
+    float f = 7.53;  // si queremeos determinar cuantos decimales queremos= ("%.2f", f)
+    double d = 12323.878987; // se utiliza  %lf
+    
+
+    return 0;
+}
