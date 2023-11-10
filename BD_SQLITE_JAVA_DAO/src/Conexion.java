84c6fb7876bb0405340772d85a83a9299f6add1c
Elton Saravia <ryuzaki12elton@gmail.com>
2023-11-10 11:20:38 +0100

    he cambiado la direccion de la base de datos

--- BD_SQLITE_JAVA_DAO/src/Conexion.java
+++ BD_SQLITE_JAVA_DAO/src/Conexion.java
@@ -1,5 +1,4 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 
 public class Conexion {
     Connection conectar;
@@ -7,7 +6,7 @@ public class Conexion {
     public Connection conectar() {
         try {
             Class.forName("org.sqlite.JDBC");
-            conectar = DriverManager.getConnection("jdbc:sqlite:BD_SQLITE_JAVA_DAO/BD.db");
+            conectar = DriverManager.getConnection("jdbc:sqlite:C:\\Users\a\Usuario\Documents\GitHub\ProyectosDam2Dual\BD_SQLITE_JAVA_DAO\BD.db");
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Error al conectar a la base de datos.");
