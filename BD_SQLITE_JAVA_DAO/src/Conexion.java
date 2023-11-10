52dc91d08700e66ddd9c64da0a0e6dbab853cfab
Elton Saravia <ryuzaki12elton@gmail.com>
2023-11-10 11:37:25 +0100

    Conexion Funcionando
    Y a funsiona la conexion y el sistema CRUD

--- BD_SQLITE_JAVA_DAO/src/Conexion.java
+++ BD_SQLITE_JAVA_DAO/src/Conexion.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Conexion {
     Connection conectar;
@@ -6,7 +7,7 @@ public class Conexion {
     public Connection conectar() {
         try {
             Class.forName("org.sqlite.JDBC");
-            conectar = DriverManager.getConnection("jdbc:sqlite:\BD.db");
+            conectar = DriverManager.getConnection("jdbc:sqlite:BD.db");
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Error al conectar a la base de datos.");
