6466a58b36fa8c0f3660cd2f943365902920b5a2
Elton Saravia <ryuzaki12elton@gmail.com>
2023-12-01 19:13:45 +0100

    main de prueba

--- Acceso_A_Datos/Academia_Futbol/src/App.java
+++ Acceso_A_Datos/Academia_Futbol/src/App.java
@@ -1,5 +1,25 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        // Crear un objeto Directivo con datos de ejemplo
+        Directivo directivo = new Directivo();
+        directivo.setId(1);
+        directivo.setCargo("Presidente");
+
+        // Crear un objeto de la clase DirectivoDAO
+        DirectivoDAO directivoDAO = new DirectivoDAO();
+
+        // Insertar el directivo en la base de datos
+        boolean exito = directivoDAO.createRecord(directivo);
+
+        // Verificar si la inserción fue exitosa
+        if (exito) {
+            System.out.println("Directivo insertado correctamente en la base de datos.");
+        } else {
+            System.out.println("Error al insertar el directivo en la base de datos.");
+        }
+
+        // Cerrar la conexión a la base de datos al finalizar
+        Conexion_BD.cerrarConexion();
     }
 }
