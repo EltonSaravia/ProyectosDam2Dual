fa1f9c1f24f8bdfdb3e808af1b810437f5812d58
Elton Saravia <ryuzaki12elton@gmail.com>
2023-12-13 18:14:44 +0100

    cambios nueva actividad en Jframe

---
+++ Desarrollo de interfaces/Netbeans_Jframe/Actividad_1_EltonSaravia/src/actividad_1_eltonsaravia/Actividad_1_EltonSaravia.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package actividad_1_eltonsaravia;
+
+/**
+ *
+ * @author alumno
+ */
+public class Actividad_1_EltonSaravia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
