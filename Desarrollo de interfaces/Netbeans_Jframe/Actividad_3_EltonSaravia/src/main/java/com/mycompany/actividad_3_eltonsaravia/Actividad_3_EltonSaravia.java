82c4852acee7daa74f3ed012c84a563b970d2187
Elton Saravia <ryuzaki12elton@gmail.com>
2024-01-05 19:09:33 +0100

    Actividad 3 empezada
    funcionan layouts

---
+++ Desarrollo de interfaces/Netbeans_Jframe/Actividad_3_EltonSaravia/src/main/java/com/mycompany/actividad_3_eltonsaravia/Actividad_3_EltonSaravia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividad_3_eltonsaravia;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Actividad_3_EltonSaravia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
