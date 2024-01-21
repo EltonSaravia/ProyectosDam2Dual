c27c64bd2768731e1fc33c6252863ec208c814ce
Elton Saravia <ryuzaki12elton@gmail.com>
2024-01-21 18:44:13 +0100

    Nuevo componente 1

---
+++ Desarrollo de interfaces/Netbeans_Jframe/Actividad_5_EltonSaravia/src/actividad_5_eltonsaravia/COMPONENTE1.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actividad_5_eltonsaravia;
+
+/**
+ *
+ * @author Usuario
+ */
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class COMPONENTE1 extends JButton implements MouseListener {
+
+    private Color originalBackgroundColor;
+    private Color hoverBackgroundColor;
+    
+    public COMPONENTE1( ) {
+
+    }
+
+    public COMPONENTE1(String text) {
+        super(text);
+        init();
+    }
+
+    private void init() {
+        // Configurar la fuente
+       // setFont(new Font("Vivaldi", Font.BOLD, 14));
+
+        // Configurar colores
+        originalBackgroundColor = new Color(100, 30, 22);
+        setBackground(originalBackgroundColor);
+        setForeground(new Color(33, 47, 60));
+
+        // Agregar el manejador de eventos del mouse
+        addMouseListener(this);
+    }
+
+    public void setHoverBackgroundColor(Color hoverColor) {
+        this.hoverBackgroundColor = hoverColor;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        if (hoverBackgroundColor != null) {
+            setBackground(hoverBackgroundColor);
+        }
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        setBackground(originalBackgroundColor);
+    }
+}
+
--- Desarrollo de interfaces/Netbeans_Jframe/Actividad_5_EltonSaravia/src/actividad_5_eltonsaravia/PantallaPrincipal.java
+++ Desarrollo de interfaces/Netbeans_Jframe/Actividad_5_EltonSaravia/src/actividad_5_eltonsaravia/PantallaPrincipal.java
@@ -29,7 +29,6 @@ public class PantallaPrincipal extends javax.swing.JFrame {
     private void initComponents() {
 
         jButtonBoton = new javax.swing.JButton();
-        enlace1 = new actividad_5_eltonsaravia.Enlace();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -51,20 +50,13 @@ public class PantallaPrincipal extends javax.swing.JFrame {
             }
         });
 
-        enlace1.setText("enlace1");
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(109, 109, 109)
-                        .addComponent(jButtonBoton, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(123, 123, 123)
-                        .addComponent(enlace1, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(109, 109, 109)
+                .addComponent(jButtonBoton, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(139, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -72,9 +64,7 @@ public class PantallaPrincipal extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(60, 60, 60)
                 .addComponent(jButtonBoton, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(64, 64, 64)
-                .addComponent(enlace1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(125, Short.MAX_VALUE))
+                .addContainerGap(202, Short.MAX_VALUE))
         );
 
         pack();
@@ -128,7 +118,6 @@ public class PantallaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private actividad_5_eltonsaravia.Enlace enlace1;
     private javax.swing.JButton jButtonBoton;
     // End of variables declaration//GEN-END:variables
 }
